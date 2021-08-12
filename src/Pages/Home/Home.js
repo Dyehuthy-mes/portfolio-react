1e0800aeaccdc781008eda10f3a40763a840c43f
Marcos Campot <campotelmarcos@gmail.com>
2021-08-12 20:46:54 -0300

    cleaning

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,12 +8,6 @@ export default function Home() {
         <div>
         <hr/>
         <Page>
-            <Card
-            avatar="https://e7.pngegg.com/pngimages/676/661/png-clipart-neon-genesis-evangelion-shinji-ikari-raising-project-neon-genesis-evangelion-shinji-ikari-raising-project-yoshiyuki-sadamoto-misato-katsuragi-mug-child-face.png" 
-            id="003356779912" 
-            name="Shinji" 
-            gender="male"
-            />
         </Page>   
         </div>
         
